56e5143096aea476acd3442570dd042bb72d4456
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-09 17:11:45 +0000

    compile

---
+++ 2023/05/07/index.js
@@ -0,0 +1,12 @@
+"use strict";
+/**
+ * Given an array of integers, return a new array such that each element
+ * at index `i` of the new array is the product of all the numbers in
+ * the original array except the one at `i`.
+ *
+ * For example, if our input was `[1, 2, 3, 4, 5]`, the expected output
+ * would be `[120, 60, 40, 30, 24]`. If our input was `[3, 2, 1]`, the
+ * expected output would be `[2, 3, 6]`.
+ */
+// hmm
+// I don't entirely understand what this one is supposed to do
---
+++ 2023/05/09/index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+ *
+ * For example, the input `[3, 4, -1, 1]` should give `2`. The input `[1, 2, 0]` should give `3`.
+ *
+ * You can modify the input array in-place.
+ */
+function firstMissing(array) {
+    array = array.sort((a, b) => a - b).filter(e => e >= 0);
+    let cache = array[0];
+    for (const e of array) {
+        if (e === cache)
+            continue;
+        if (e - 1 > cache) {
+            // a number was skipped
+            return cache + 1;
+        }
+        else
+            cache = e;
+    }
+    // if nothing was skipped,
+    return cache + 1;
+}
+exports.default = firstMissing;
+console.log(firstMissing([1, 2, 0]));
