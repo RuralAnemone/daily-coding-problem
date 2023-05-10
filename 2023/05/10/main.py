f42b7985543d73e72f650f836050fb009a1d65d5
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-10 21:11:44 +0000

    solve may 10th!

---
+++ 2023/05/10/main.py
@@ -0,0 +1,17 @@
+"""
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that
+pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons, implement car and cdr:
+"""
+def cons(a, b):
+    """given"""
+    def pair(f):
+        return f(a, b)
+    return pair
+
+# I feel like this is a python thing
+# idk
+# with js you'd just use arrays
+# if I'm understanding the problem correctly
+# ugh idk
