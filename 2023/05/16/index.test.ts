b326aecdf6e9e96425869731d1a223b66cfbc452
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-18 19:04:54 +0000

    more tests!! idk it's kinda hard to write tests for something you know has no edge cases

--- 2023/05/16/index.test.ts
+++ 2023/05/16/index.test.ts
@@ -6,3 +6,21 @@ describe('given test(s)', () => {
 		expect(autocomplete('de', ['dog', 'deer', 'deal'])).toEqual(expect.arrayContaining(['deer', 'deal']));
 	})
 })
+
+describe('my tests', () => {
+	test('not found', () => {
+		expect(autocomplete('a', ['dog', 'deer', 'deal'])).toEqual(expect.arrayContaining([]));
+	})
+	test('empty', () => {
+		expect(autocomplete('a', [])).toEqual(expect.arrayContaining([]));
+	})
+	test('exact match', () => {
+		expect(autocomplete('dog', ['dog', 'deer', 'deal'])).toEqual(expect.arrayContaining(['dog']));
+	})
+	test('overtyping', () => {
+		expect(autocomplete('dogg', ['dog', 'deer', 'deal'])).toEqual(expect.arrayContaining([]));
+	})
+	test('empty string', () => {
+		expect(autocomplete('', ['dog', 'deer', 'deal'])).toEqual(expect.arrayContaining(['dog', 'deer', 'deal']));
+	})
+})
