10aef48ccc43a02bf2aa7a75401e06cbd0231b5e
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-19 17:02:56 +0000

    something like that

--- 2023/05/18/index.ts
+++ 2023/05/18/index.ts
@@ -8,33 +8,24 @@
  * with k distinct characters is "bcb".
  */
 function longestSubstring(k: number, s: string): string {
-	// yeah this is just standard string manipulation x sliding window
-	let longest = '';
-	let current = '';
-	let distinct = 0;
-	let i = 0;
-	let j = 0;
-	while (j < s.length) {
-		if (distinct <= k) {
-			if (current.indexOf(s[j]) === -1) {
-				distinct++;
+	let candidates: string[] = [];
+
+	for (let i = 0; i < s.length; i++) {
+		let diffChars: string[] = [s[i]];
+		let str: string = "";
+		for (let i = 0; diffChars.length <= k; i++) {
+			const char = s[i];
+			if (diffChars.includes(char)) {
+				str += char;
+				continue;
+			} else {
+				diffChars.push(char);
 			}
-			current += s[j];
-			j++;
-		} else {
-			if (current.length > longest.length) {
-				longest = current;
-			}
-			current = '';
-			distinct = 0;
-			i++;
-			j = i;
 		}
+		candidates.push(str);
 	}
-	if (current.length > longest.length) {
-		longest = current;
-	}
-	return longest;
+
+	return candidates.sort((a, b) => b.length - a.length)[0];
 }
 
 export { longestSubstring };
