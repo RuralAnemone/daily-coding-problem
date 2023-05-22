419d846217fc0d9b1576948fc4f849650d03db9f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-05-21 19:46:48 -0500

    comments for failed testing

--- 2023/05/19/index.test.ts
+++ 2023/05/19/index.test.ts
@@ -4,6 +4,9 @@ import { monteCarloPi } from '.'
 describe('self-testing', () => {
 	test('three decimal places', () => {
 		expect(monteCarloPi(10000000)).toBeCloseTo(Math.PI, 3)
+		// hello! if you see this it probably means the test failed :)
+		// due to the nature of monte carlo methods, it's bound to happen sometime
+		// should be pretty rare so consider yourself lucky hehe
 	})
 })
 
