e970e9a38442711084b4e00333d016c033d0217c
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-22 16:33:28 +0000

    fit to screen width idk

--- 2023/05/22/index.ts
+++ 2023/05/22/index.ts
@@ -5,7 +5,7 @@
  * 
  * The string "`dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"` represents:
  * 
- * ```py
+ * ```python ew
  * dir
  *     subdir1
  *     subdir2
@@ -15,7 +15,7 @@
  * 
  * The string `"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"` represents:
  * 
- * ```py
+ * ```python ew
  * dir
  *     subdir1
  *         file1.ext
@@ -24,11 +24,16 @@
  *         subsubdir2
  *             file2.ext
  * ```
- * The directory `dir` contains two sub-directories `subdir1` and `subdir2`. `subdir1` contains a file `file1.ext` and an empty second-level sub-directory `subsubdir1`. `subdir2` contains a second-level sub-directory `subsubdir2` containing a file `file2.ext`.
+ * The directory `dir` contains two sub-directories `subdir1` and `subdir2`.
+ * `subdir1` contains a file `file1.ext` and an empty second-level sub-directory `subsubdir1`.
+ * `subdir2` contains a second-level sub-directory `subsubdir2` containing a file `file2.ext`.
  * 
- * We are interested in finding the longest (number of characters) absolute path to a file within our file system. For example, in the second example above, the longest absolute path is `"dir/subdir2/subsubdir2/file2.ext"`, and its length is 32 (not including the double quotes).
+ * We are interested in finding the longest (number of characters) absolute path to a file within our file system.
+ * For example, in the second example above, the longest absolute path is `"dir/subdir2/subsubdir2/file2.ext"`, and
+ * its length is 32 (not including the double quotes).
  * 
- * Given a string representing the file system in the above format, return the length of the longest absolute path to a file in the abstracted file system. If there is no file in the system, return 0.
+ * Given a string representing the file system in the above format, return the length of the
+ * longest absolute path to a file in the abstracted file system. If there is no file in the system, return 0.
  * 
  * Note:
  * 
@@ -36,4 +41,8 @@
  * 
  * The name of a directory or sub-directory will not contain a period.
  */
-function markdwonMoment() {}
+function longestPath(dirString: string): number {
+	// ok so it's basically just searching for the longest string of \t that ends with \w+\.\w+
+	// hehe this feels like cheating :)
+	return 0;
+}
