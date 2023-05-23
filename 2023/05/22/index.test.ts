f0f4242a9a3d4b89c5ee3f0a1915d5dc6df1a348
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-23 00:31:56 +0000

    tests maybe

--- 2023/05/22/index.test.ts
+++ 2023/05/22/index.test.ts
@@ -1,1 +1,8 @@
 import 'jest'
+import { longestPath } from '.'
+
+describe('given test(s)', () => {
+	test('first example', () => {
+		expect(longestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext")).toBe()
+	})
+})
