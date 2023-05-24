8d5d6f68eb9a1d2cab3d9652237ff7a2997b23a8
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-24 16:09:59 +0000

    add jsdoc

---
+++ 2023/05/24/index.ts
@@ -0,0 +1,9 @@
+/**
+ * This problem was asked by Facebook.
+ * 
+ * A builder is looking to build a row of `N` houses that can be of `K` different colors.
+ * He has a goal of minimizing cost while ensuring that no two neighboring houses are of the same color.
+ * 
+ * Given an `N` by `K` matrix where the `nᵗʰ` row and `kᵗʰ` column represents the cost to build the `nᵗʰ` house
+ * with `kᵗʰ` color, return the minimum cost which achieves this goal.
+ */
