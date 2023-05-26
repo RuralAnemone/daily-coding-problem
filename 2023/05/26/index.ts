3441a8142a3c1a2f58ea3c4d646c487d48c35d9d
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-05-26 15:49:57 -0500

    create index.ts

---
+++ 2023/05/26/index.ts
@@ -0,0 +1,17 @@
+/**
+ * This problem was asked by Snapchat.
+ * 
+ * Given an array of time intervals (start, end) for classroom lectures (possibly overlapping),
+ * find the minimum number of rooms required.
+ * 
+ * For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+ * 
+ * ---
+ * 
+ * huh this is an interesting but actually useful problem
+ * 
+ * I think there was something like it on the ap exam
+ *
+ * I MEAN OF COURSE THERE WASN'T
+ *
+ * */
